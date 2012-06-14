d0ffa9035643dc4fa54f499ece6ef43d9c8688b1
Tilo Buschmann <tibu@cbs.mpg.de>
2012-06-14 14:52:10 +0200

    vplitbeta ordentlich aufgeräumt

--- src/new/vsplitbeta/vsplitbeta.cpp
+++ src/new/vsplitbeta/vsplitbeta.cpp
@@ -21,6 +21,8 @@
 #include <viaio/VImage.h>
 #include <viaio/mu.h>
 #include <viaio/option.h>
+#include <boost/concept_check.hpp>
+#include <boost/concept_check.hpp>
 
 using std::cerr;
 using std::cout;
@@ -28,6 +30,9 @@ using std::endl;
 
 extern "C" void getLipsiaVersion(char*,size_t);
 
+// Map from beta to class
+typedef std::map<int,int> mapT; 
+  
 // My own version of VReadHistory that does not suck as much
 VAttrList _VReadHistory_(VAttrList *list) { 
   VAttrListPosn posn;
@@ -47,6 +52,45 @@ VAttrList _VReadHistory_(VAttrList *list) {
   return history_list;
 }
 
+void read_parse_conversion_file(VString conversion_file,mapT &conversion_map) {
+  std::ifstream file(conversion_file);
+  std::string   line;
+
+  while(std::getline(file, line)) {
+    std::stringstream   linestream(line);
+    int                 val1;
+    int                 val2;
+
+    linestream >> val1 >> val2;
+    conversion_map[val1] = val2;
+  }
+  file.close();
+}
+
+bool is_beta_image(VImage &image) {
+    VString name;
+    VGetAttr(VImageAttrList(image), "name", NULL, VStringRepn, &name);
+    return(0 == strcmp(name,"BETA"));
+}
+
+bool converte_beta_to_class(VImage &image, mapT &conversion_map, int &beta, int &conversion_value) {
+  VShort beta_value;
+  VGetAttr(VImageAttrList(image), "beta", NULL, VShortRepn, &beta_value);
+  mapT::iterator it = conversion_map.find(beta_value);
+  if (it == conversion_map.end())
+    return false;
+  
+  conversion_value  = it->second;
+  beta              = beta_value;
+  
+  return true;
+}
+
+std::string construct_filename(VString output_base,int beta,int svm_class) {
+  std::stringstream filename_stream;
+  filename_stream << output_base << "_" << std::setw(4) << std::setfill('0') << beta << "_" << svm_class << ".v";
+  return(filename_stream.str());
+}
 
 int main (int argc,char *argv[]) {
   /**************************
@@ -62,9 +106,8 @@ int main (int argc,char *argv[]) {
    * Parse command line parameters *
    *********************************/
   FILE *input_file;
-
-  VString output_base = NULL;
-  VString conversion_file = NULL;
+  VString output_base = NULL; // Extrated images will be stored in files with the name outputbase_betanr_class.v
+  VString conversion_file = NULL; // The file that contains the conversion beta->class
 
   static VOptionDescRec program_options[] = {
     {"base", VStringRepn, 1, &output_base, VRequiredOpt, NULL, "basename of extracted images" },
@@ -74,30 +117,8 @@ int main (int argc,char *argv[]) {
  
   cerr << "Base filename: " << output_base << endl; 
 
-  /************************
-   * Read Conversion file *
-   ************************/ 
-
-  // From beta to class
-  typedef std::map<int,int> mapT; 
   mapT conversion_map;
-
-  std::ifstream file(conversion_file);
-  std::string   line;
-
-  while(std::getline(file, line)) {
-    std::stringstream   linestream(line);
-    int                 val1;
-    int                 val2;
-
-    linestream >> val1 >> val2;
-
-    conversion_map[val1] = val2;
-  }
-
-  /******************************************
-   * Read image file and extract image data *
-   ******************************************/
+  read_parse_conversion_file(conversion_file,conversion_map);
 
   VAttrList attribute_list  = VReadFile(input_file, NULL);
   fclose(input_file);
@@ -112,60 +133,46 @@ int main (int argc,char *argv[]) {
     if (VGetAttrRepn(&position) != VImageRepn)
       continue;
 
+    // Extract this image
+    VImage image = NULL;
+    VGetAttrValue(&position,NULL,VImageRepn,&image);
+    
+    // Retrieve name of image and do nothing if image is not a beta map
+    if (!is_beta_image(image))
+      continue;
 
-    VImage selected_image = NULL;
-
-    VGetAttrValue(&position,NULL,VImageRepn,&selected_image);
-    VString name;
-    VGetAttr(VImageAttrList(selected_image), "name", NULL, VStringRepn, &name);
-
-    if (0 == strcmp(name,"BETA")) {
-        
-      // Find this beta in the conversion map
-      VShort beta_value;
-      VGetAttr(VImageAttrList(selected_image), "beta", NULL, VShortRepn, &beta_value);
-
-      mapT::iterator it = conversion_map.find(beta_value);
-
-      if (it != conversion_map.end()) {
-        int conversion_value = it->second;
+    // Find this beta in the beta->class conversion map, do nothing if not found
+    int beta,svm_class;
+    if (!converte_beta_to_class(image,conversion_map,beta,svm_class))
+      continue;
+    
+    std::string filename = construct_filename(output_base,beta,svm_class);
+    
+    VSetAttr(VImageAttrList(image),"class",NULL,VShortRepn,svm_class);
+    
+    /*******************************
+      * Save result into vista file *
+      *******************************/
+    FILE *output_file = fopen(filename.c_str(), "w");
+    if (NULL != output_file) {
       
-        std::stringstream filename_stream;
-        filename_stream << output_base << "_" << std::setw(4) << std::setfill('0') << beta_value << ".v";
-        std::string filename = filename_stream.str();
-        cerr << filename << endl;
-     
-        VSetAttr(VImageAttrList(selected_image),"class",NULL,VShortRepn,conversion_value);
-
-        /*******************************
-         * Save result into vista file *
-         *******************************/
-
-        FILE *output_file;
-        output_file = fopen(filename.c_str(), "w");
-        if (NULL != output_file) {
-
-          VAttrList out_list = VCreateAttrList();
-
-          VAppendAttr(out_list,"image",NULL,VImageRepn,selected_image);
-
-          // This aweful VHIstory function has a side effect and does indeed change the original attribute list!
-          if (first_history) {
-            VHistory(VNumber(program_options),program_options,argv[0],&attribute_list,&out_list);
-            first_history = false;
-          } else {
-            VAttrList history_list = _VReadHistory_(&attribute_list);
-            VPrependAttr(out_list,"history",NULL,VAttrListRepn,history_list);
-          }
-          VWriteFile(output_file, out_list);
-
-          fclose(output_file);
-        } else {
-          cerr << "Some error occurred" << endl;
-        }
-
+      VAttrList out_list = VCreateAttrList();
+      
+      VAppendAttr(out_list,"image",NULL,VImageRepn,image);
+      
+      // This aweful VHIstory function has a side effect and does indeed change the original attribute list!
+      if (first_history) {
+        VHistory(VNumber(program_options),program_options,argv[0],&attribute_list,&out_list);
+        first_history = false;
+      } else {
+        VAttrList history_list = _VReadHistory_(&attribute_list);
+        VPrependAttr(out_list,"history",NULL,VAttrListRepn,history_list);
       }
-
+      VWriteFile(output_file, out_list);
+      
+      fclose(output_file);
+    } else {
+      cerr << "Some error occurred" << endl;
     }
   }
 }
